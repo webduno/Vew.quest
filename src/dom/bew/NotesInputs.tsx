ba672da1e46d3b6d65d7219d9d8090ab6eb78d8b
Abraham Duno <tresduno@gmail.com>
2025-04-22 18:09:18 -0400

    tg 2

--- src/dom/bew/NotesInputs.tsx
+++ src/dom/bew/NotesInputs.tsx
@@ -24,7 +24,7 @@ export const NotesInputs = ({
   return (<>
     <div className='flex-col flex-align-stretch tx-altfont-2'>
       <textarea className='flex-1 bord-r-15 pa-4' 
-        rows={20}
+        rows={12}
         value={value}
         onChange={handleChange}
         style={{
