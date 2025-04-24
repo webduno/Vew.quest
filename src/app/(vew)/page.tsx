017c8a8bb3ed3dda751044801dac1c5c42e5eee8
Abraham Duno <tresduno@gmail.com>
2025-04-24 12:56:35 -0400

    reorg 1

--- src/app/(vew)/page.tsx
+++ src/app/(vew)/page.tsx
@@ -1,34 +1,28 @@
 'use client';
-import { useState, useCallback, useEffect } from 'react';
-import { useLSPlayerId, usePlayerStats } from '@/../script/state/hook/usePlayerStats';
-import { useFetchedStats } from '@/script/state/context/FetchedStatsContext';
+import { useState, useEffect } from 'react';
 
-import { Tooltip } from 'react-tooltip';
-import { BewUserStatsSummary } from '../../dom/bew/BewUserStatsSummary';
-import { isMobile } from '../../../script/utils/platform/mobileDetection';
-import { random10CharString } from "../../../script/utils/platform/random10CharString";
-import { LandingProfileActionButton } from '@/dom/bew/LandingProfileActionButton';
+import { useLSPlayerId } from '@/../script/state/hook/usePlayerStats';
+import { random10CharString } from "@/../script/utils/platform/random10CharString";
+
+import { BewUserStatsSummary } from '@/dom/bew/BewUserStatsSummary';
+import { LandingProfileActionButton } from '@/dom/organ/vew/LandingProfileActionButton';
+import { LandingUsernameInput } from '@/dom/organ/vew/LandingUsernameInput';
 import { BewPurpleBtn } from '@/dom/bew/BewBtns';
 import { NavigationHeaderBar } from '@/dom/bew/NavigationHeaderBar';
-import { VersionTag } from '@/dom/bew/VersionTag';
+import { VewLandingGraphic } from '@/dom/organ/vew/VewLandingGraphic.tsx';
 
 
 export default function TrainingPage() {
   const { typedUsername, setTypedUsername, setPlayerId, sanitizePlayerId } = useLSPlayerId();
-  // const { crvObjects } = useFetchedStats();
+  const [userExists, setUserExists] = useState(0);
+  const [streak, setStreak] = useState(0);
+  const [potentialStreak, setPotentialStreak] = useState(0);
+  const [avgResult, setAvgResult] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
 
-
-  
-const [ wndwTg, s__wndwTg] = useState<any>(null);
-const [ telegram_id, s__telegram_id] = useState<string | null>(null);
-const [userExists, setUserExists] = useState(0);
-const [streak, setStreak] = useState(0);
-const [potentialStreak, setPotentialStreak] = useState(0);
-const [avgResult, setAvgResult] = useState(0);
-
-useEffect(() => {
   const fetchUserCount = async () => {
     if (!typedUsername) return;
+    setIsLoading(true);
     try {
       const response = await fetch(`/api/supabase/count?storageKey=${typedUsername}`);
       const data = await response.json();
@@ -40,15 +34,17 @@ useEffect(() => {
       }
     } catch (error) {
       console.error('Error fetching user count:', error);
+    } finally {
+      setIsLoading(false);
     }
   };
-
-  fetchUserCount();
-}, [typedUsername]);
+  
+  useEffect(() => {
+    if (isLoading) { return }
+    fetchUserCount();
+  }, [typedUsername]);
 
   const handleStart = async () => {
-
-
     if ( typedUsername) {
       setPlayerId(sanitizePlayerId(typedUsername));
     } else {
@@ -60,94 +56,53 @@ useEffect(() => {
   };
 
 
-
-// useEffect(() => {
-//   setTelegram();
-// }, [])
-
-  // useEffect(() => {
-  //   if (!LS_playerId) {
-  //     return }
-  //   fetchMailboxRequests();
-  // }, [LS_playerId, fetchMailboxRequests]);
-
   return (
     <div className='w-100 h-100  flex-col flex-justify-start'>
       <div className='w-100  flex-col  '>
         
-        <NavigationHeaderBar linkList={<>
-            <a href="/about" className='nodeco' style={{ color: "#AFAFAF" }}>
-              <div>About <VersionTag /></div>
-            </a>
-          </>}
-        />
+        <NavigationHeaderBar />
+
         <div className='flex-wrap gap-8 px-4 ' style={{ height: "70vh", }}>
-          <div className='flex-col '>
-            <div className='Q_xs_sm py-4'></div>
 
-            <img src="/bew/cleaneyes.png" alt="tool_bg3"
-            className={'pointer hover-jump pos-abs noselect noverflow block  Q_xs_pt-8 '+ (isMobile() ? 'w-100px' : 'w-150px')} 
-            />
+          <VewLandingGraphic />
 
-            <img src="/bew/starsbg2.jpeg" alt="tool_bg4"
-              className={'pointer bord-r-50 noverflow noselect block '+(isMobile() ? 'w-150px' : 'w-250px')} 
-            />
-
-          </div>
-          <div className= {' tx-altfont-2 tx-bold _dd b flex-col gap-2    flex-col w-300px'}
-            style={{color: "#777777",}}
-          >
-            <div className='flex-col _dd g tx-center tx-lgx landing -title'>Gamified <br /> step-by-step lessons for remote viewing</div>
+          <div className= {' tx-altfont-2  flex-col gap-2  w-300px'}>
+            <div className='tx-center tx-lgx tx-bold tx-gg'>
+              Gamified <br /> step-by-step lessons for remote viewing
+            </div>
             <div className='flex-col _dd r'>
               {userExists > 0 && (<>
                 <div className="flex-row flex-justify-between flex-align-center">
                   <BewUserStatsSummary minified calculatedStats={{
                     potentialStreak: potentialStreak,
                     streak: streak,
-                    dailyProgress: 0,
-                    dailyGoal: 0,
-                    isLoading: false,
-                    error: null,
                     averageResult: avgResult
                   }} 
                   crvObjects_length={userExists}
                   />
                 </div>
               </>)}
-              <div>
-                <input 
-                  type="text" 
-                  className='bord-r-10 tx-altfont-2 py-2 mb-2 px-3 tx-center'
-                  placeholder='Enter your name'
-                  style={{
-                    border: "1px solid #E5E5E5",
-                  }}
-                  value={typedUsername}
-                  onChange={(e) => { setTypedUsername(sanitizePlayerId(e.target.value)) }}
-                  onKeyDown={(e) => {
-                    if (e.key === "Enter") {
-                      handleStart();
-                    }
-                  }}
-                />
+              
+              <LandingUsernameInput 
+                typedUsername={typedUsername}
+                setTypedUsername={setTypedUsername}
+                handleStart={handleStart}
+              />
+
+              <div className="flex-row flex-align-start">
+                {userExists > 0 && (
+                  <div className='flex-row'>
+                    <LandingProfileActionButton typedUsername={typedUsername} handleStart={handleStart} />
+                  </div>
+                )}
+                <div>
+                  <BewPurpleBtn text="Start" onClick={handleStart} />
+                </div>
               </div>
-
-              <div className="flex-row flex-align-start"
-              >
-                {userExists > 0 && (<>
-                <div  className='flex-row'>
-                <LandingProfileActionButton typedUsername={typedUsername} handleStart={handleStart} />
-                </div>
-                </>)}
-                <div >
-
-                <BewPurpleBtn text="Start" onClick={handleStart}  />
-                
-                
-                </div>
-                </div>
             </div>
           </div>
+
+
         </div>
       </div>
     </div>
