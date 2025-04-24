30f9acae4e8b5854e1155934c47ff2b4d8d844b0
Abraham Duno <tresduno@gmail.com>
2025-04-23 23:20:40 -0400

    min daily goal

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -309,7 +309,7 @@ style={{
   <div className='tx-bold tx-lg mb-2'>Daily Goals</div>
   <div className='flex-col gap-2 flex-align-start'>
     <div>Current Streak: {streak}</div>
-    <div>Completed Goal: {crvObjects.filter(obj => obj.created_at.split('T')[0] === new Date().toISOString().split('T')[0]).length >= 5 ? '✅' : "❌"} ({userStats.dailyGoals.requests > 5 ? 5 : userStats.dailyGoals.requests} / 5)</div>
+    <div>Completed Goal: {crvObjects.filter(obj => obj.created_at.split('T')[0] === new Date().toISOString().split('T')[0]).length >= 5 ? '✅' : "❌"} ({userStats.dailyGoals.requests > 3 ? 3 : userStats.dailyGoals.requests} / 3)</div>
     <div>Viewed Today: {userStats.dailyGoals.requests}</div>
     <div>Avg Accuracy: {userStats.dailyGoals.accuracy > 0 ? userStats.dailyGoals.accuracy.toFixed(3) : 'N/A'}%</div>
     <div>Best Today: {userStats.dailyGoals.bestAccuracy > 0 ? userStats.dailyGoals.bestAccuracy.toFixed(3) : 'N/A'}%</div>
